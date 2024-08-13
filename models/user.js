0b18b93005b60ff08bd452a2f2761e4483436065
Akeron44 <oniakeron@gmail.com>
2024-08-13 16:37:43 +0200

    Authentication

--- models/user.js
+++ models/user.js
@@ -1,11 +1,44 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, minLength: 3, maxLength: 55, required: true },
   lastName: { type: String, minLength: 3, maxLength: 55, required: true },
-  email: { type: String, minLength: 3, maxLength: 55, required: true },
-  password: { type: String, minLength: 8, maxLength: 50, required: true },
-  isManager: { type: Boolean, required: true },
+  email: { type: String, minLength: 5, maxLength: 55, required: true },
+  password: { type: String, minLength: 8, maxLength: 255, required: true },
+  isManager: { type: Boolean, default: false },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      isManager: this.isManager,
+    },
+
+    process.env.JWT_PRIVATE_KEY,
+    { expiresIn: "10h" }
+  );
+  return token;
+};
+
 const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
+    email: Joi.string().min(5).required().email(),
+    password: Joi.string().min(8).max(255).required(),
+    isManager: Joi.boolean(),
+  });
+
+  return schema.validate(user);
+}
+
 exports.User = User;
+exports.validate = validateUser;
--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,11 @@
-
 const express = require("express");
-
+const signup = require("../routes/signup");
+const login = require("../routes/login");
 
 require("express-async-errors");
 
-
-
 module.exports = function (app) {
   app.use(express.json());
- 
-
+  app.use("/api/signup", signup);
+  app.use("/api/login", login);
 };
