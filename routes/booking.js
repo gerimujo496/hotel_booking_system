787b2f554c90d5ac936550c2579cf65ba7bbca9c
gerimujo <geri.mujo@softup.co>
2024-08-14 23:03:50 +0200

    changed the structure of the queries

--- routes/booking.js
+++ routes/booking.js
@@ -13,11 +13,12 @@ router.post("/requestToBook/", isClient, async (req, res) => {
   if (!arrivalDate || !departureDate || !roomId || !user._id)
     return res.status(400).send(`Invalid Request`);
 
-  const existingBooking = await Booking.findOne({
+  const existingBookingBody = {
     arrivalDate: { $gte: arrivalDate },
     departureDate: { $lt: departureDate },
-    roomId: roomId,
-  });
+    roomId,
+  };
+  const existingBooking = await Booking.findOne(existingBookingBody);
 
   const room = await Room.findOne({ _id: roomId });
   if (!room) res.status(400).send(`The room does not exists !`);
@@ -25,12 +26,13 @@ router.post("/requestToBook/", isClient, async (req, res) => {
   if (existingBooking && existingBooking.isApproved)
     return res.status(400).send(`The room is not available on those dates !`);
 
-  const booking = new Booking({
+  const bookingBody = {
     userId: user._id,
-    roomId: roomId,
-    arrivalDate: arrivalDate,
-    departureDate: departureDate,
-  });
+    roomId,
+    arrivalDate,
+    departureDate,
+  };
+  const booking = new Booking(bookingBody);
 
   const newBooking = await booking.save();
   res.status(201).send(newBooking);
@@ -40,18 +42,17 @@ router.delete("/requestToBook/:bookingId", isClient, async (req, res) => {
   const { bookingId } = req.params;
   if (!bookingId) return res.status(400).send(`Invalid Request`);
 
-  const existingBooking = await Booking.findOne({
+  const bookingBody = {
     _id: bookingId,
-  });
+  };
+  const existingBooking = await Booking.findOne(bookingBody);
 
   if (!existingBooking)
     return res
       .status(400)
       .send(`The booking with the id: ${bookingId} does not exists  !`);
 
-  const bookingDelete = await Booking.deleteOne({
-    _id: bookingId,
-  });
+  const bookingDelete = await Booking.deleteOne(bookingBody);
   res.status(200).send(bookingDelete);
 });
 
@@ -59,11 +60,23 @@ router.get("/requestToBook/:bookingId", isClient, async (req, res) => {
   const { bookingId } = req.params;
   if (!bookingId) return res.status(400).send(`Invalid Request`);
 
-  const booking = await Booking.findOne({
-    _id: bookingId,
-  })
-    .populate({ path: "userId", select: "firstName lastName email" })
-    .populate({ path: "roomId", select: "type number description" });
+  const queryBody = {};
+
+  queryBody.bookingBody = { _id: bookingId };
+
+  queryBody.populateUserId = {
+    path: "userId",
+    select: "firstName lastName email",
+  };
+
+  queryBody.populateRoomId = {
+    path: "roomId",
+    select: "type number description",
+  };
+
+  const booking = await Booking.findOne(queryBody.bookingBody)
+    .populate(queryBody.populateUserId)
+    .populate(queryBody.populateRoomId);
 
   if (!booking)
     return res
@@ -81,42 +94,56 @@ router.put("/requestToBook/:bookingId", isClient, async (req, res) => {
   if (!arrivalDate || !departureDate || !roomId || !bookingId)
     return res.status(400).send(`Invalid Request !`);
 
-  const booking = await Booking.findOne({ _id: bookingId });
+  const queryBody = {};
+  queryBody.bookingBody = { _id: bookingId };
+
+  queryBody.availableRoomBody = {
+    arrivalDate: { $gte: arrivalDate },
+    departureDate: { $lt: departureDate },
+    roomId: bookingId,
+  };
+
+  queryBody.newBookingBody = {
+    arrivalDate,
+    departureDate,
+    roomId,
+  };
+  const booking = await Booking.findOne(queryBody.bookingBody);
 
   if (!booking)
     return res
       .status(400)
       .send(`The booking with the id: ${bookingId} does not exists  !`);
 
-  const availableRoom = await Booking.findOne({
-    arrivalDate: { $gte: arrivalDate },
-    departureDate: { $lt: departureDate },
-    roomId: bookingId,
-  });
+  const availableRoom = await Booking.findOne(queryBody.availableRoomBody);
 
   if (availableRoom && availableRoom.isApproved)
     return res.status(400).send(`The room is not available on those dates !`);
 
   const newBooking = await Booking.findOneAndUpdate(
-    { _id: bookingId },
-    {
-      arrivalDate: arrivalDate,
-      departureDate: departureDate,
-      roomId: roomId,
-    },
+    queryBody.bookingBody,
+    queryBody.newBookingBody,
     { new: true }
   );
-  
+
   res.status(200).send(newBooking);
 });
 
 router.get("/getCurrentClient", isManager, async (req, res) => {
-  const currentClient = await Booking.find({
+  const queryBody = {};
+
+  queryBody.queryBody.currentClientBody = {
     arrivalDate: { $lte: Date.now() },
-  }).populate({
+  };
+
+  queryBody.populateUserId = {
     path: "userId",
     select: "firstName, lastName, email",
-  });
+  };
+
+  const currentClient = await Booking.find(
+    queryBody.currentClientBody
+  ).populate(queryBody.populateUserId);
 
   res.status(200).send(currentClient.userId);
 });
@@ -125,13 +152,17 @@ router.get("/getBookingHistory/:userId", isClient, async (req, res) => {
   const { userId } = req.params;
   if (!userId) return res.status(400).send(`Invalid Request !`);
 
-  const bookings = await Booking.find({
-    userId: userId,
-    arrivalDate: { $lte: Date.now() },
-  }).populate({
+  const queryBody = {};
+
+  queryBody.bookings = { userId, arrivalDate: { $lte: Date.now() } };
+  queryBody.populateRoomId = {
     path: "roomId",
     select: "type numberOfBeds description",
-  });
+  };
+
+  const bookings = await Booking.find(queryBody.bookings).populate(
+    queryBody.populateRoomId
+  );
 
   res.status(200).send(bookings);
 });
